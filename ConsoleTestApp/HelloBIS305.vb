6367f048375616428e7e28cdf69d136b66243eac
m2web <m2web@yahoo.com>
2019-08-21 17:16:49 -0400

    Updated the module to get the user name and display it.

--- ConsoleTestApp/HelloBIS305.vb
+++ ConsoleTestApp/HelloBIS305.vb
@@ -3,6 +3,10 @@
     Sub Main()
         Dim message As New Message()
         Console.WriteLine(message.CreateMessage())
+        Console.Write("What is your name? ")
+        Dim strName As String = Console.ReadLine()
+        Console.WriteLine(message.SayHello(strName))
+        Console.WriteLine("Press any key to exit.")
         Console.ReadKey()
     End Sub
 
